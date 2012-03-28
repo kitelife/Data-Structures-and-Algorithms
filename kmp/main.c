73ad986180087f88c92fdfba007827c36d2f4b2f
xiayf <sas.198708@gmail.com>
2012-03-28 14:44:12 +0800

    ...

--- kmp/main.c
+++ kmp/main.c
@@ -4,18 +4,28 @@
 int main()
 {
 	SString mainstr = "aaabaaaab";
-	char *chars = "aaaab";
-	int len = strlen(chars);
+	unsigned char *chars = (unsigned char *)"aaaab";
+	unsigned char *chstemp = chars;
+	int len = 0;
+	while(*chstemp){
+		len ++;
+		chstemp++;
+	}
 	int temp = len;
+	int temp2 = len;
+	
 	SString paternstr;
 	paternstr[0] = len;
-	char *p = paternstr + 1;
-	char *ch = chars;
+	
+	unsigned char *p = paternstr + 1;
+	unsigned char *ch = chars;
 	while(temp)
 	{
 		*p++ = *ch++;
 		temp--;
 	}
+	*p = '\0';
+
 	int *next =(int *)malloc(MAXSTRLEN * sizeof(int));
 	get_next(paternstr, next);
 	while(len)
@@ -24,6 +34,15 @@ int main()
 		len--;
 	}
 	printf("\n%d\n",Index_KMP(mainstr, paternstr, 1, next));
+
+	int *nextval = (int *)malloc(MAXSTRLEN * sizeof(int));
+	get_nextval(paternstr, nextval);
+	while(temp2)
+	{
+		printf("%d\n", nextval[temp2]);
+		temp2--;
+	}
+	printf("\n%d\n", Index_KMP(mainstr, paternstr, 1, nextval));
 	return 0;
 }
 
--- kmp/sstring.h
+++ kmp/sstring.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define MAXSTRLEN	255
 typedef unsigned char SString[MAXSTRLEN + 1];	// 0号单元存放串的长度
